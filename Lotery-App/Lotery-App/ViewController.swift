a05486ee599d91798bc5b25f3f9d0c8c21b1ebfa
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-01-08 12:56:47 -0300

    ready the final layout
    Terminado o layout.

--- Lotery-App/Lotery-App/ViewController.swift
+++ Lotery-App/Lotery-App/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbGameType: UILabel!
+    @IBOutlet weak var scGameType: UISegmentedControl!
+    
+    @IBOutlet var balls: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func generateGame() {
+    }
+    
 }
 
